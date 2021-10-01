9baa8ca618067dcf9168258156e8a3551ab0a99d
filkur <filipkuranda@gmail.com>
2021-10-01 10:16:04 +0200

    modify UserInput

--- src/DTO/User/Input/UserInput.php
+++ src/DTO/User/Input/UserInput.php
@@ -4,11 +4,28 @@ declare(strict_types=1);
 
 namespace App\DTO\User\Input;
 
+use App\Entity\User\User;
+
 class UserInput
 {
-    public ?string $nickname;
+    public ?string $id = null;
 
-    public ?string $email;
+    public ?string $nickname = null;
 
-    public ?string $password;
+    public ?string $email = null;
+
+    public ?string $password = null;
+
+
+    public static function createFromEntity(User $user):  self
+    {
+        $self = new self();
+
+        $self->id = $user->getIdString();
+        $self->email = $user->getEmail();
+        $self->nickname = $user->getNickname();
+        $self->password = $user->getPassword();
+
+        return $self;
+    }
 }
