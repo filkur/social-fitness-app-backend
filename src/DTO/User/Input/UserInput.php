0f236175aacbee52bcd9faa98c2b3b71f989f405
filkur <filipkuranda@gmail.com>
2021-10-01 14:10:13 +0200

    add denormalization groups

--- src/DTO/User/Input/UserInput.php
+++ src/DTO/User/Input/UserInput.php
@@ -5,17 +5,34 @@ declare(strict_types=1);
 namespace App\DTO\User\Input;
 
 use App\Entity\User\User;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 class UserInput
 {
+    /**
+     * @Groups({"user:put", "user:patch"})
+     */
     public ?string $id = null;
 
+    /**
+     * @Groups({"user:post", "user:put"})
+     */
     public ?string $nickname = null;
 
+    /**
+     * @Groups({"user:post", "user:put"})
+     */
     public ?string $email = null;
 
+    /**
+     * @Groups({"user:post","user:put", "user:patch"})
+     */
     public ?string $password = null;
 
+    /**
+     * @Groups({"user:patch"})
+     */
+    public ?string $oldPassword = null;
 
     public static function createFromEntity(User $user):  self
     {
