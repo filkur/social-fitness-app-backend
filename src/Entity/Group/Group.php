35c05f3d8f6cafc8f24a5afa9e164a16f0958c3e
filkur <filipkuranda@gmail.com>
2021-10-12 23:14:03 +0200

    create Group

---
+++ src/Entity/Group/Group.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Group;
+
+use App\Entity\Traits\Timestamp\Timestamp;
+use App\Entity\Traits\Timestamp\TimestampInterface;
+use App\Entity\Traits\UlidTrait;
+use App\Entity\User\User;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\Group\GroupRepository;
+use App\Entity\GroupMember\GroupMember;
+
+/**
+ * @ORM\Entity(
+ *     repositoryClass=GroupRepository::class
+ * )
+ */
+class Group implements TimestampInterface
+{
+    use UlidTrait;
+    use Timestamp;
+
+    /**
+     * @ORM\Column(
+     *      type="string",
+     *      length=50
+     * )
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(
+     *      type="string",
+     *      length=200
+     * )
+     */
+    private string $description;
+
+    /**
+     * @ORM\ManyToOne(
+     *     targetEntity=User::class,
+     *     inversedBy="groups"
+     * )
+     */
+    private User $owner;
+
+    /**
+     * @ORM\OneToMany(
+     *     targetEntity=GroupMember::class,
+     *      mappedBy="group"
+     * )
+     */
+    private Collection $groupMembers;
+
+    public function __construct()
+    {
+        $this->groupMembers = new ArrayCollection();
+    }
+}
