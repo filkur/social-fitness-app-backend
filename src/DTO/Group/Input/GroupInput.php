c48763f050d59f0c7d425150d07d01115d98b6da
filkur <filipkuranda@gmail.com>
2021-10-13 11:42:19 +0200

    set deserialization groups

--- src/DTO/Group/Input/GroupInput.php
+++ src/DTO/Group/Input/GroupInput.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace App\DTO\Group\Input;
 
+use App\Entity\Group\Group;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class GroupInput
 {
     /**
+     * @Groups({"group:patch"})
+     */
+    public ?string $id = null;
+
+    /**
+     * @Groups({"group:post", "group:patch"})
      * @Assert\NotBlank()
      * @Assert\Length(
      *     max=50
@@ -20,6 +28,7 @@ class GroupInput
     public ?string $name = null;
 
     /**
+     * @Groups({"group:post", "group:patch"})
      * @Assert\NotBlank()
      * @Assert\Length(
      *     max=50
@@ -29,4 +38,16 @@ class GroupInput
      * )
      */
     public ?string $description = null;
+
+    public static function createFromEntity(
+        Group $group
+    ): self {
+        $self = new self();
+
+        $self->id = $group->getIdString();
+        $self->name = $group->getName();
+        $self->description = $group->getDescription();
+
+        return $self;
+    }
 }
