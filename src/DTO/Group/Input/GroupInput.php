53e0046a35267c88a42a06cebb53e1ddc5ec2f24
filkur <filipkuranda@gmail.com>
2021-10-13 11:17:39 +0200

    add GroupInput

--- src/DTO/Group/Input/GroupInput.php
+++ src/DTO/Group/Input/GroupInput.php
@@ -4,6 +4,29 @@ declare(strict_types=1);
 
 namespace App\DTO\Group\Input;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class GroupInput
 {
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     max=50
+     * )
+     * @Assert\Type(
+     *     type="string"
+     * )
+     */
+    public ?string $name = null;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     max=50
+     * )
+     * @Assert\Type(
+     *     type="string"
+     * )
+     */
+    public ?string $description = null;
 }
