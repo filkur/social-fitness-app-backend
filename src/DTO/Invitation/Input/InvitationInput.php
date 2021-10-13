e9417c4c0dc80f7abaf52ebfad9f3446a1c92575
filkur <filipkuranda@gmail.com>
2021-10-13 13:52:25 +0200

    check if group exist

--- src/DTO/Invitation/Input/InvitationInput.php
+++ src/DTO/Invitation/Input/InvitationInput.php
@@ -3,7 +3,15 @@
 declare(strict_types=1);
 
 namespace App\DTO\Invitation\Input;
+use App\Validator\PropertySetted\IsIdSet;
+use App\Entity\Group\Group;
 
 class InvitationInput
 {
+    /**
+     * @IsIdSet(
+     *     targetEntity=Group::class
+     * )
+     */
+    public string $groupId;
 }
