2dc7dc4ce6522bf0e3f9de76fdb31380d6842506
filkur <filipkuranda@gmail.com>
2021-10-13 11:18:26 +0200

    create GroupOutput

--- src/DTO/Group/Output/GroupOutput.php
+++ src/DTO/Group/Output/GroupOutput.php
@@ -4,6 +4,20 @@ declare(strict_types=1);
 
 namespace App\DTO\Group\Output;
 
+use App\DTO\Invitation\Output\InvitationOutput;
+use App\DTO\User\Output\UserOutput;
+
 class GroupOutput
 {
+    public string $id;
+
+    public string $name;
+
+    public string $description;
+
+    public UserOutput $owner;
+
+    public ?array $groupMembers;
+
+    public ?InvitationOutput $invitation = null;
 }
