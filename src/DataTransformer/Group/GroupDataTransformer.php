b1a12de2045dfb86206b506cc3835976c10f0628
filkur <filipkuranda@gmail.com>
2021-10-13 11:19:45 +0200

    create Group Data transformer

---
+++ src/DataTransformer/Group/GroupDataTransformer.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataTransformer\Group;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\DTO\Group\Output\GroupOutput;
+use App\DTO\Invitation\Output\InvitationOutput;
+use App\DTO\User\Output\UserOutput;
+use App\Entity\Group\Group;
+
+class GroupDataTransformer implements DataTransformerInterface
+{
+    /**
+     * @param Group $object
+     */
+    public function transform($object, string $to, array $context = [])
+    {
+        $output = new GroupOutput();
+
+        $output->id = $object->getIdString();
+        $output->owner = UserOutput::createFromUser(
+            $object->getOwner()
+        );
+        $output->name = $object->getName();
+        $output->description = $object->getDescription();
+        $output->groupMembers = $object->getGroupMembersArray();
+        if ($object->getInvitation() === null){
+            return $output;
+        }
+        $output->invitation = InvitationOutput::createFromInvitation(
+            $object->getInvitation()
+        );
+
+        return $output;
+    }
+
+
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        return $to === GroupOutput::class && $data instanceof Group;
+    }
+}
