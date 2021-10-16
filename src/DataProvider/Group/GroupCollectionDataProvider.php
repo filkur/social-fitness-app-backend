fc33bbc914bfd7941c5bf56c0e8064feb1d60050
filkur <filipkuranda@gmail.com>
2021-10-16 17:48:51 +0200

    return groups where user is member

--- src/DataProvider/Group/GroupCollectionDataProvider.php
+++ src/DataProvider/Group/GroupCollectionDataProvider.php
@@ -7,18 +7,26 @@ namespace App\DataProvider\Group;
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\Entity\Group\Group;
+use App\Entity\GroupMember\GroupMember;
 use App\Entity\User\User;
+use App\Repository\Group\GroupRepository;
+use App\Repository\GroupMember\GroupMemberRepository;
 use App\Utils\User\UserGetter;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class GroupCollectionDataProvider
     implements ContextAwareCollectionDataProviderInterface, RestrictedDataProviderInterface
 {
     private UserGetter $userGetter;
 
+    private GroupMemberRepository $groupMemberRepository;
+
     public function __construct(
-        UserGetter $userGetter
+        UserGetter $userGetter,
+        GroupMemberRepository $groupMemberRepository
     ) {
         $this->userGetter = $userGetter;
+        $this->groupMemberRepository = $groupMemberRepository;
     }
 
     public function getCollection(string $resourceClass, string $operationName = null, array $context = [])
@@ -26,7 +34,27 @@ class GroupCollectionDataProvider
         /** @var User $owner */
         $owner = $this->userGetter->get();
 
-        return $owner->getGroups();
+        $ownerGroupsArray = $owner->getGroups()->toArray();
+
+
+        $ownerGroupMembers = new ArrayCollection(
+            $this->groupMemberRepository->findBy([
+                'user' => $owner
+            ])
+        );
+
+        $ownerGroupMemberArray = [];
+        foreach ($ownerGroupMembers as $groupMember) {
+            /** @var GroupMember $groupMember */
+            $ownerGroupMemberArray[] = $groupMember->getGroup();
+        }
+
+        return new ArrayCollection(
+            array_merge(
+                $ownerGroupsArray,
+                $ownerGroupMemberArray
+            )
+        );
     }
 
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
