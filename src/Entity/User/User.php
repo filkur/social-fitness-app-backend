fdd05b1ff671bf70adb61c022cc475af2d739a7e
filkur <filipkuranda@gmail.com>
2021-10-22 21:43:16 +0200

    repair User class

--- src/Entity/User/User.php
+++ src/Entity/User/User.php
@@ -148,7 +148,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, Timesta
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
+        //  guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
