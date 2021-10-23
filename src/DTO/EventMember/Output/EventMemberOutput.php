c67b3f6a8c675a962efe29ee9b7140a76e072cdd
filkur <filipkuranda@gmail.com>
2021-10-23 14:48:19 +0200

    add EventMember output

--- src/DTO/EventMember/Output/EventMemberOutput.php
+++ src/DTO/EventMember/Output/EventMemberOutput.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace App\DTO\EventMember\Output;
 
+use App\DTO\User\Output\UserOutput;
+use Symfony\Component\Serializer\Annotation\Groups;
+
 class EventMemberOutput
 {
+    /**
+     * @Groups({"eventMember:base"})
+     */
+    public UserOutput $user;
+
+    /**
+     * @Groups({"eventMember:base"})
+     */
+    public ?array $activities;
 }
