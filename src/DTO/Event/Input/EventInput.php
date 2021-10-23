c284600aab072b556226274e09924baa4c37680e
filkur <filipkuranda@gmail.com>
2021-10-23 14:27:49 +0200

    add validators

--- src/DTO/Event/Input/EventInput.php
+++ src/DTO/Event/Input/EventInput.php
@@ -6,57 +6,138 @@ namespace App\DTO\Event\Input;
 
 use App\Entity\Event\Event;
 use App\Utils\Date\DateHelper;
+use App\Validator\PropertySetted\IsIdSet;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Entity\Group\Group;
 
 class EventInput
 {
     /**
      * @Groups({"event:patch"})
+     * @Assert\NotBlank(
+     *     groups={"event:update"}
+     * )
+     * @Assert\Type(
+     *     groups={"event:update"},
+     *     type="string"
+     * )
      */
     public string $id;
 
     /**
      * @Groups({"event:post"})
+     * @IsIdSet(
+     *     groups={"event:create"},
+     *     targetEntity=Group::class
+     * )
      */
     public string $groupId;
 
     /**
      * @Groups({"event:post", "event:patch"})
+     * @Assert\NotBlank(
+     *     groups={"event:create", "event:update"}
+     * )
+     * @Assert\Type(
+     *     groups={"event:create", "event:update"},
+     *     type="string"
+     * )
+     * @Assert\Length(
+     *     groups={"event:create", "event:update"},
+     *     max=40
+     * )
      */
     public string $name;
 
     /**
      * @Groups({"event:post", "event:patch"})
+     * @Assert\NotBlank(
+     *     groups={"event:create", "event:update"}
+     * )
+     * @Assert\Type(
+     *     groups={"event:create", "event:update"},
+     *     type="string"
+     * )
+     * @Assert\Length(
+     *     groups={"event:create", "event:update"},
+     *     max=200
+     * )
      */
     public string $description;
 
     /**
      * @Groups({"event:post", "event:patch"})
+     * @Assert\Type(
+     *     groups={"event:create", "event:update"},
+     *     type="integer"
+     * )
+     * @Assert\GreaterThan(
+     *     groups={"event:create", "event:update"},
+     *     value="-1"
+     * )
      */
     public int $pointGoal = 0;
 
     /**
      * @Groups({"event:post", "event:patch"})
+     * @Assert\Type(
+     *     groups={"event:create", "event:update"},
+     *     type="integer"
+     * )
+     * @Assert\GreaterThan(
+     *     groups={"event:create", "event:update"},
+     *     value="-1"
+     * )
      */
     public int $pointPerRep = 0;
 
     /**
      * @Groups({"event:post", "event:patch"})
+     * @Assert\Type(
+     *     groups={"event:create", "event:update"},
+     *     type="integer"
+     * )
+     * @Assert\GreaterThan(
+     *     groups={"event:create", "event:update"},
+     *     value="-1",
+     * )
      */
     public int $pointPerMinute = 0;
 
     /**
      * @Groups({"event:post", "event:patch"})
+     * @Assert\NotBlank(
+     *     groups={"event:create", "event:update"}
+     * )
+     * @Assert\Type(
+     *     groups={"event:create", "event:update"},
+     *     type="string"
+     * )
      */
     public string $startDate;
 
     /**
      * @Groups({"event:post", "event:patch"})
+     * @Assert\NotBlank(
+     *     groups={"event:create", "event:update"}
+     * )
+     * @Assert\Type(
+     *     groups={"event:create", "event:update"},
+     *     type="string"
+     * )
      */
     public string $endDate;
 
     /**
      * @Groups({"event:post", "event:patch"})
+     * @Assert\NotBlank(
+     *     groups={"event:create", "event:update"}
+     * )
+     * @Assert\Type(
+     *     groups={"event:create", "event:update"},
+     *     type="string"
+     * )
      */
     public string $eventType;
 
